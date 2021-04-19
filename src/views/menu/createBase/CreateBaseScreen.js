2fc1748296ebe0ca2ab637bec862daee16a3b54f
GaboRocker1992 <gabo.borbor.1992@gmail.com>
2021-04-19 15:28:41 -0500

    gabo - Updating to 19 04 2021 15:29

--- src/views/menu/createBase/CreateBaseScreen.js
+++ src/views/menu/createBase/CreateBaseScreen.js
@@ -7,10 +7,11 @@ import {
     TextField,
     Button,
 } from "@material-ui/core";
-
 import {withStyles} from "@material-ui/core/styles";
-
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+
+//Sweet Alert
+import swal from 'sweetalert';
 
 //Style
 import {CreateBaseStyles} from "./CreateBaseStyle";
@@ -19,6 +20,53 @@ import {CreateBaseStyles} from "./CreateBaseStyle";
 import {CreateBaseStrings} from "./CreateBaseStrings";
 
 class CreateBaseScreen extends Component{
+    constructor(props) {
+        super(props);
+        // No llames this.setState() aquí!
+        this.state = {
+           openDrawer: false,
+           currentAction: null,
+
+           //Cartera Data
+           dataBaseName: "",
+           dataBaseDescription: "",
+           dataBaseFileName: "",
+           dataBaseCsvFile: null,
+        };
+    }
+
+    onCreateDataBaseClick = () => {
+        if(this.state.dataBaseName.length === 0){
+            swal("Ingrese nombre de Cartera.", {icon: "error"});
+            return;
+        }
+
+        if(this.state.dataBaseDescription.length === 0){
+            swal("Ingrese descripción para la cartera.", {icon: "error"});
+            return;
+        }
+
+        if(this.state.dataBaseFileName.length === 0){
+            swal("Seleccione un archivo csv.", {icon: "error"});
+            return;
+        }
+    }
+
+    onSelectFile = (event) => {
+        const file = event.target.files[0];
+        if(!file.name.endsWith(".csv")){
+            swal("Imagen debe estar en formato csv.", {icon: "error"});
+            return;
+        }
+
+        this.setState({
+            dataBaseFileName: file.name,
+            dataBaseCsvFile: file
+        });
+
+    }
+
+
     render(){
         const { classes } = this.props;
         return(
@@ -29,6 +77,8 @@ class CreateBaseScreen extends Component{
                     className = {classes.textFieldComponent}
                     fullWidth
                     variant = "outlined" 
+                    value = {this.state.dataBaseName}
+                    onChange = {(event) => this.setState({dataBaseName: event.target.value})}
                     label = {CreateBaseStrings.createDataBaseHintName}
                 />
 
@@ -36,6 +86,8 @@ class CreateBaseScreen extends Component{
                     className = {classes.textFieldComponent}
                     fullWidth
                     variant = "outlined" 
+                    value = {this.state.dataBaseDescription}
+                    onChange = {(event) => this.setState({dataBaseDescription: event.target.value})}
                     label = {CreateBaseStrings.createDataBaseHintDescription}
                     multiline
                     rows = {3}
@@ -47,10 +99,17 @@ class CreateBaseScreen extends Component{
                         fullWidth
                         variant = "outlined" 
                         size = "small"
+                        value = {this.state.dataBaseFileName}
+                        onChange = {(event) => this.setState({dataBaseFileName: event.target.value})}
                         label = {CreateBaseStrings.fileNameHintText}
                         disabled = {true}
                     />
-                    <input accept="image/*" className={classes.uploadFileInput} id="icon-button-file" type="file" />
+                    <input 
+                        accept=".csv" 
+                        className={classes.uploadFileInput} 
+                        id="icon-button-file" type="file" 
+                        onChange = {(event) => this.onSelectFile(event)}
+                    />
                     <label htmlFor="icon-button-file">
                         <Button
                             variant = "contained"
@@ -66,6 +125,7 @@ class CreateBaseScreen extends Component{
                 <Button
                     className = {[classes.createDataBaseButton, classes.textFieldComponent]}
                     variant = "contained"
+                    onClick = {() => this.onCreateDataBaseClick()}
                 >
                     {CreateBaseStrings.createDataBaseButtonText}      
                 </Button>
