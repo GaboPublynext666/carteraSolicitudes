59584972ba97e2e8b3c451bc1eca567739656ef9
GaboRocker1992 <gabo.borbor.1992@gmail.com>
2021-04-19 14:20:02 -0500

    gabo - Updating to 19 04 2021 14:20

--- src/strings.js
+++ src/strings.js
@@ -17,44 +17,4 @@ export const Strings = ({
     loginButtonText: "Iniciar Sesión",
     missingUsername: "Ingrese su Usuario",
     missingPassword: "Ingrese su contraseña",
-
-
-    //Business Module
-    businessModuleTitle: "Información de Negocio",
-    businessName: "Nombre de Negocio",
-    businessIdentification: "RUC / RISE",
-    businessAddress: "Dirección",
-    businessPhone: "Teléfono",
-    businessMail: "Email",
-    businessFacebookLink: "Enlace de Facebook",
-    businessInstagramLink: "Enlace de Instagram",
-    businessTwitterLink: "Enlace de Twitter",
-    businessDataEditButtonText: "Editar",
-    businessDataSaveButtonText: "Guardar",
-    businessDataCancelButtonText: "Cancelar",
-
-    //Products Module
-    createProductButton: "Crear Producto",
-    
-    viewProductButton: "Ver",
-    editProdutButton: "Editar",
-    deleteProductButton: "Eliminar",
-
-    //Product Dialog
-    dialogProductName: "Nombre de Producto",
-    dialogProductDescription: "Descripción de Producto",
-    dialogCategoryName: "Categoría de Producto",
-    //User Module
-    createUserButton: "Crear Usuario",
-    //User Dialog
-    dialogUserName: "Nombre del Usuario",
-    dialogUserLastName: "Apellido del Usuario",
-    dialogUserIdentification: "Identificacion del Usuario",
-    dialogUserPassword: "Contraseña/usuario",
-    dialogUserVerifyPassword: "Antigua Contraseña",
-    dialogUserPhone: "Telefono del Usuario",
-    dialogUserMail: "Correo del Usuario",
-    dialogUserGenre: "Sexo del Usuario",
-    dialogUserRol: "Rol del Usuario",
-
 });
---
+++ src/views/menu/createBase/CreateBaseStyle.js
@@ -0,0 +1,42 @@
+export  const CreateBaseStyles = theme => ({
+    container: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+
+    title: {
+        color: "#000000",
+        fontSize: "2vw",
+        fontWeight: "bold",
+        alignText: "center",
+    },
+
+    textFieldComponent: {
+        marginTop: "2%",
+    },
+
+    createDataBaseButton: {
+        textTransform: "none",
+        backgroundColor: "#000000",
+        color: "#ffffff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+    uploadFileComponent: {
+        marginTop: "2%",
+        display: "flex",
+        flexDirection: "row",
+        width: "100%",
+    },
+
+    uploadFileInput: {
+        display: "none"
+    },
+
+    uploadFileButton: {
+        textTransform: "none",
+        width: "10vw"
+    },
+});
